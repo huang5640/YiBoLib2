0f526b6f942e7bf77c294e3322964f01bac9f10b
jason <huang5640@live.com>
2016-01-03 20:44:17 -0800

    latest commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,6 @@ $(document).on('page:fetch', function() {
 $('#loading').show();
 });
 
-$(document).on('page:fetch', function() {
-$('#loading').show();
+$(document).on('page:change', function() {
+$('#loading').hide();
 });
